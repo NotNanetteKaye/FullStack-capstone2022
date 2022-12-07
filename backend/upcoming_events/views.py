85252b4ea93e4638ff5b6f6594c232b6d2143d55
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-07 02:43:23 -0600

    Created app upcoming_events urls.py

--- backend/upcoming_events/views.py
+++ backend/upcoming_events/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from .models import UpcomingEvent
+from .serializers import UpcomingEventSerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def show_all_upcoming_events(request):
+    events = UpcomingEvent.objects.all
+    serializer = UpcomingEventSerializer(events, many=True)
+    return Response('serializer.data')
