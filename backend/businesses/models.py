3ff5cf1caa1d3fb3de099f12a586e2aa0ff0cef8
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-08 16:20:59 -0600

    Created businesses app and wrote in models.py

---
+++ backend/businesses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from authentication.models import User
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here.
+
+class UpcomingEvent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    type = models.CharField(max_length=300)
+    address = models.CharField(max_length=300)
+    thumbnail = models.CharField(max_length=280)
+    link = models.CharField(max_length=300)
