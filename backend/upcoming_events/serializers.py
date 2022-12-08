a40b955a7eb60bb592304ad5068fa95d7b2def41
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-08 15:06:26 -0600

    Updated upcoming_events app serializers.py

--- backend/upcoming_events/serializers.py
+++ backend/upcoming_events/serializers.py
@@ -7,3 +7,5 @@ class UpcomingEventSerializer(serializers.ModelSerializer):
         model = UpcomingEvent
         fields = ['id', 'user', 'title', 'when', 'address', 'description', 'image', 'link', 'user_id']
         depth = 1
+
+    user_id = serializers.IntegerField(write_only=True)
