56fc599f5be9c41304eb308bce66d96915b1dd61
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-08 16:36:14 -0600

    Registered business app's urls.py to project

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    path('api/upcoming_events/', include('upcoming_events.urls'))
+    path('api/upcoming_events/', include('upcoming_events.urls')),
+    path('api/businesses/', include('businesses.urls')),
 ]
