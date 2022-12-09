097fa6e9a828364c885e192e52292f2313331780
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-08 21:01:05 -0600

    Created spotify_artists app file

--- backend/artists/models.py
+++ backend/artists/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Artist(models.Model):
-    id = models.CharField(max_length=280)
+    spotify_id = models.CharField(max_length=280)
     image = models.CharField(max_length=300)
     artist_name = models.CharField(max_length=280)
     genre = models.CharField(max_length=280)
