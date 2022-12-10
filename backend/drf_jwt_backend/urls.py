787d79c4c9f62d5600a03595db969e1cc8c6a9c4
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-09 22:20:03 -0600

    Updated project's urls.py

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('api/cars/', include('cars.urls')),
     path('api/upcoming_events/', include('upcoming_events.urls')),
     path('api/businesses/', include('businesses.urls')),
-    path('api/artists/', include('spotify_artists.urls')),
+    path('api/artists/', include('sp_artists.urls')),
 ]
