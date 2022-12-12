c875fd6489d6ec883c5135373f27495a1ccd4e57
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-12 11:26:13 -0600

    Deleted spotify_artists app

--- backend/spotify_artists/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpotifyArtistsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spotify_artists'
--- backend/spotify_artists/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.artist_list),
-    path('<pk>/', views.artist_detail),
-]
