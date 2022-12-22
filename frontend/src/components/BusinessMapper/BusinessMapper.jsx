c3e0393aae6b39b03ad878dcdaa6031dcd7ff613
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-22 17:49:25 -0600

    Revised BusinessMapper.jsx to match suggested edit

--- frontend/src/components/BusinessMapper/BusinessMapper.jsx
+++ frontend/src/components/BusinessMapper/BusinessMapper.jsx
@@ -4,7 +4,14 @@ import BusinessPresenter from '../BusinessPresenter/BusinessPresenter';
 const BusinessMapper = ({businesses}) => {
     return (
         <div>
-            {businesses.map(business => <BusinessPresenter key={business.local_results.places.map((business, index) => console.log(index))} business={business.local_results.places}/>)}
+            {console.log(businesses)}
+
+            {businesses.map((business, index) => {
+            return (
+                <BusinessPresenter key={index} places={business.local_results.places} />
+            )
+        })}
+        
         </div>
     )
 }
--- frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
+++ frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../DarlingDreamsPage/DarlingDreams.css"
 
 const DarlingDreamsPage = () => {
     return (
