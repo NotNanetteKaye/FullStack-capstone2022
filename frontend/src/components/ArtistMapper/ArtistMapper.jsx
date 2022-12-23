398877e52a10a96c5421aaa3c25c7352aff5e627
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-23 05:08:37 -0600

    Updated ArtistPresenter.jsx button functionality

--- frontend/src/components/ArtistMapper/ArtistMapper.jsx
+++ frontend/src/components/ArtistMapper/ArtistMapper.jsx
@@ -4,7 +4,8 @@ import ArtistPresenter from "../ArtistPresenter/ArtistPresenter"
 const ArtistMapper = ({artists}) => {
     return (
         <div>
-            {artists.map(e => <ArtistPresenter key={e.id} artist={e}/>)}
+            {artists.map(e => <ArtistPresenter key={e.id} artist={e} spotify_id={e.id}
+            img={e.images[0]?.url} artist_name={e.name} genre={e.genres}/>)}
         </div>
     );
 }
--- frontend/src/components/ArtistPresenter/ArtistPresenter.jsx
+++ frontend/src/components/ArtistPresenter/ArtistPresenter.jsx
@@ -2,35 +2,44 @@ import axios from "axios"
 import React, { useState } from 'react';
 import useAuth from "../../hooks/useAuth"
 
-const ArtistPresenter = ({artist}) => {
-
-    const [faveArtist, setFaveArtist] = useState([])
+const ArtistPresenter = ({artist, spotify_id, img, artist_name, genre}) => {
     const [user, token] = useAuth();
 
-    async function saveFavoriteArtist() {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let newFaveArtist = {
+            spotify_id: '1' ,
+            image: 'img' ,
+            artist_name: 'nan' ,
+            genre: 'genre' ,
+            URI: 'URI',
+            user_id: 1 ,
+        };
         try {
-            const response = await axios.get('http://127.0.0.1:8000/api/artists/1/', {
+            const response = await axios.post('http://127.0.0.1:8000/api/artists/', 
+            newFaveArtist, 
+            {
                 headers: {Authorization: "Bearer " + token},
-            });
-            if (response.status == 200) {
-                alert("Successfully favorited a new artist!")
             }
-            setFaveArtist(response.data)
+        );
+        https://github.com/NotNanetteKaye/youtube-clone.git
+        console.log(newFaveArtist);
         } catch (error) {
-            console.log(error.response);
+         console.log(error.response);
         }
     }
 
 
     return (
         <div>
-            <img src={artist.images[0]?.url} height={artist.images.height} width={artist.images.width} alt=''/>
-            <h1>{artist.name}</h1>
-            <p>Genre(s): {artist.genres}</p>
-            <p>Popularity: {artist.popularity}</p>
-            <p>{artist.external_urls.spotify}</p>
-            <button onClick={() => saveFavoriteArtist()}>Save me to your faves! </button>
-
+            <form onSubmit={handleSubmit}>
+                <img src={artist.images[0]?.url} height={artist.images.height} width={artist.images.width} alt=''/>
+                <h1>{artist.name}</h1>
+                <p>Genre(s): {artist.genres}</p>
+                <p>Popularity: {artist.popularity}</p>
+                <p>{artist.external_urls.spotify}</p>
+                <button type='submit'>Save me to your faves! </button>
+            </form>
         </div>
     )
 }
