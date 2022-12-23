cc15f3abc33c866a57524982048aa2d750753bdc
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-23 01:09:54 -0600

    ArtistPresenter.jsx's save button works! ✅

--- frontend/src/components/ArtistPresenter/ArtistPresenter.jsx
+++ frontend/src/components/ArtistPresenter/ArtistPresenter.jsx
@@ -1,7 +1,25 @@
+import axios from "axios"
+import React, { useState } from 'react';
+import useAuth from "../../hooks/useAuth"
 
 const ArtistPresenter = ({artist}) => {
 
-    
+    const [faveArtist, setFaveArtist] = useState([])
+    const [user, token] = useAuth();
+
+    async function saveFavoriteArtist() {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/artists/1/', {
+                headers: {Authorization: "Bearer " + token},
+            });
+            if (response.status == 200) {
+                alert("Successfully favorited a new artist!")
+            }
+            setFaveArtist(response.data)
+        } catch (error) {
+            console.log(error.response);
+        }
+    }
 
 
     return (
@@ -11,7 +29,7 @@ const ArtistPresenter = ({artist}) => {
             <p>Genre(s): {artist.genres}</p>
             <p>Popularity: {artist.popularity}</p>
             <p>{artist.external_urls.spotify}</p>
-            <button>Save me</button>
+            <button onClick={() => saveFavoriteArtist()}>Save me to your faves! </button>
 
         </div>
     )
