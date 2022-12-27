2942835c94e277fab3ef5c9131eab8293a649013
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-27 17:48:58 -0600

    Beginning to write logic in FavoritesPage.js

--- frontend/src/pages/FavoritesPage/FavoritesPage.js
+++ frontend/src/pages/FavoritesPage/FavoritesPage.js
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+import useAuth from "../../hooks/useAuth"
 
 const FavoritesPage = () => {
+    const [faveArtists, setFaveArtists] = useState([]);
+    const [user, token] = useAuth();
+
+    useEffect(() => {
+        GetAllFaveArtists();
+    }, []);
+
+    async function GetAllFaveArtists() {
+        try {
+            let response = await axios.get(`http://127.0.0.1:8000/api/artists/${user}/`,
+            {
+                headers: {Authorization: "Bearer " + token},
+            });
+            setFaveArtists(response.data);
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+
+
     return (
         <div>
-
         </div>
     )
 }
