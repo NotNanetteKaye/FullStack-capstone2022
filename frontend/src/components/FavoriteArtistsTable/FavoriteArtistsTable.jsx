023a0684f023c5a5b3a8e67cc255cf3b8cbb2be1
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-28 02:04:57 -0600

    Passed Artists Django API Data to child component

---
+++ frontend/src/components/FavoriteArtistsTable/FavoriteArtistsTable.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// const FavoriteArtistsTable = ({artistsData}) => {
+//     return ( 
+//     <div>
+
+//     </div>
+//     );
+// }
+ 
+export default function FavoriteArtistsTable(props) {
+    console.log(props.artists)
+
+    return (
+        <>
+        </>
+    )
+}
--- frontend/src/pages/FavoritesPage/FavoritesPage.js
+++ frontend/src/pages/FavoritesPage/FavoritesPage.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios"
 import useAuth from "../../hooks/useAuth"
+import FavoriteArtistsTable from '../../components/FavoriteArtistsTable/FavoriteArtistsTable';
 
 const FavoritesPage = () => {
     const [user, token] = useAuth();
     const [artists, getArtists] = useState('')
 
-    const getAllArtists = () => {
-        axios.get(`http://127.0.0.1:8000/api/artists/${user}/`,
-        {
-            headers: {Authorization: "Bearer " + token},
-        }).then ((response) => {
-            const allArtists = response.data;
-            getArtists(allArtists)
-        })
-        .catch(error => console.log(error.response.data))
+    const getAllArtists = async () => {
+        try {
+            let response = await axios.get('http://127.0.0.1:8000/api/artists/', 
+            {
+                headers: {Authorization: "Bearer " + token},
+            });
+            getArtists(response.data);
+    } catch (error) {
+        console.log(error.message)
+    }
     }
 
     useEffect(() => {
@@ -23,9 +25,9 @@ const FavoritesPage = () => {
 
     return (
         <div>
-            <FavoriteArtistsTable favoriteArtists = {artists} />
+            <FavoriteArtistsTable artists={artists} />
         </div>
     )
 }
 
-export default FavoritesPage
+export default FavoritesPage;
