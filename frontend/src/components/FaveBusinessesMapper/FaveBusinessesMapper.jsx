ed7a15a236aba70b6bd11a18fbd05792c7214cc2
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-29 19:07:51 -0600

    Wrote logic in FaveBusinessMapper.jsx

--- frontend/src/components/FaveBusinessesMapper/FaveBusinessesMapper.jsx
+++ frontend/src/components/FaveBusinessesMapper/FaveBusinessesMapper.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export default function FaveBusinessesMapper(props) {
-    
+
+    const {faveBusinesses, businesses} = props
+
+    if (businesses.length > 0) {
+        return (
+            businesses.map((business, index) => {
+                return (
+                    <div>
+                        <FaveBusinessPresenter key={business.index} id={business.id} name={business.name} />
+                    </div>
+                )
+            })
+        )
+    }
+
 }
