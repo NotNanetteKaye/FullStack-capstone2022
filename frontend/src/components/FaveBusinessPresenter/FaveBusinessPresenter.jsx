12d3e0113f36ae93b923f831380aeab66ddd87d1
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-12-29 19:09:12 -0600

    Created FaveBusinessesPresenter.jsx

---
+++ frontend/src/components/FaveBusinessPresenter/FaveBusinessPresenter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+import useAuth from '../../hooks/useAuth';
+
+const FaveBusinessPresenter = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default FaveBusinessPresente
+<div>
+    
+</div>r;
