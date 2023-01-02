a49345482f2aeeb5a9e4222e4bba60eb2231880c
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-01-02 15:21:54 -0600

    Updated DarlingDreams.css to look like WireFrame

--- frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
+++ frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
@@ -3,9 +3,9 @@ import "../DarlingDreamsPage/DarlingDreams.css"
 
 const DarlingDreamsPage = () => {
     return (
-        <div>
+        <div className='DarlingDreams'>
             <h1>DARLING DREAMS</h1>
-            <h2>A blog dedicated to uplifting (Texas) black/brown artists, entrepreneurs, and upcoming events.</h2>
+            <h2>A blog dedicated to uplifting Texas black/brown artists, entrepreneurs, and upcoming events.</h2>
         </div>
     )
 }
