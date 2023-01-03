47be571584fd0e74e22244603b68bfc1d03f828d
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-01-03 15:18:24 -0600

    Fixed format in DarlingDreams.css

--- frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
+++ frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
@@ -4,8 +4,12 @@ import "../DarlingDreamsPage/DarlingDreams.css"
 const DarlingDreamsPage = () => {
     return (
         <div className='DarlingDreams'>
-            <h1>DARLING DREAMS</h1>
-            <h2>A blog dedicated to uplifting Texas black/brown artists, entrepreneurs, and upcoming events.</h2>
+            <div id="fixed">
+                <h1>DARLING DREAMS</h1>
+                <h2>A blog dedicated to uplifting Texas black/brown artists, entrepreneurs, and upcoming events.</h2>
+            </div>
+            <div id="remaining">
+            </div>
         </div>
     )
 }
