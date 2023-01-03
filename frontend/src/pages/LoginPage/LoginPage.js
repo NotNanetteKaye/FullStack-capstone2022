9a7a4e0b20c9a45d51aca19bebcd475d86a48840
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-01-03 13:24:51 -0600

    Altered format for LoginPage in Apps.css & ''.css

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -20,33 +20,37 @@ const LoginPage = () => {
 
   return (
     <div className="container">
-      <h1>L  O  G  I  N</h1>
-      <h2>to see all your saved favorites.</h2>
-      <form className="form" onSubmit={handleSubmit}>
-        <label className="Field">
-          Username:{" "}
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label className="Field">
-          Password:{" "}
-          <input
-            type="text"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
-        {isServerError ? (
-          <p className="error">Login failed, incorrect credentials!</p>
-        ) : null}
-         <button>Login</button>
-        <Link to="/register">click here to register</Link>
-      </form>
+      <div className="loginPage">
+        <div className="loginPageContent">
+          <h1>L  O  G  I  N</h1>
+          <h2>to see all your saved favorites.</h2>
+          <form className="form" onSubmit={handleSubmit}>
+            <label className="Field">
+              Username:{" "}
+              <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleInputChange}
+              />
+            </label>
+            <label className="Field">
+              Password:{" "}
+              <input
+                type="text"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </label>
+            {isServerError ? (
+              <p className="error">Login failed, incorrect credentials!</p>
+            ) : null}
+            <button>Login</button>
+            <Link to="/register">click here to register</Link>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
