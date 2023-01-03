f8cd765b44e808366a2c45282480bcf567a9d440
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-01-03 15:33:34 -0600

    Fixed DarlingDreams.js format

--- frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
+++ frontend/src/pages/DarlingDreamsPage/DarlingDreams.js
@@ -8,7 +8,7 @@ const DarlingDreamsPage = () => {
                 <h1>DARLING DREAMS</h1>
                 <h2>A blog dedicated to uplifting Texas black/brown artists, entrepreneurs, and upcoming events.</h2>
             </div>
-            <div id="remaining">
+            <div id="remaining1">
             </div>
         </div>
     )
