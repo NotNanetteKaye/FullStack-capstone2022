2fd69eaced2ce48b22180adab64dbe99a30a4618
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-01-02 22:27:34 -0600

    Final edits in FaveArtistPresenter.css complete

--- frontend/src/components/FaveArtistPresenter/FaveArtistPresenter.jsx
+++ frontend/src/components/FaveArtistPresenter/FaveArtistPresenter.jsx
@@ -20,10 +20,19 @@ const ArtistPresenter = (props) => {
 
     return ( 
         <div>
-            <form onSubmit={DeleteFave}>
-                <h3>{props.name}</h3>
-                <div className="faveDiv">
-                    <button>Delete.</button>
+            <form onSubmit={DeleteFave} className='favePresenters'>
+                <div className="together">
+                    <div className="left">
+                        <img src={props.pic} alt='' height='100mm' width='120mm'/>
+                    </div>
+                    <div className="title">
+                        <h3>{props.name}</h3>
+                    </div>
+                    <div className="button">
+                        <div className="faveDiv">
+                            <button>Delete.</button>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
